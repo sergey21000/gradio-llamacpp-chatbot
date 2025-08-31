d3847af53a58865e760c47cfda6883c3564dd4d0
sergey21000 <67040429+sergey21000@users.noreply.github.com>
2025-08-31 23:54:12 +0300

    Update test_chatbot.py

--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent))
 from typing import Iterator
+sys.path.append(str(Path(__file__).parent.parent))
 
 import pytest
 import gradio as gr
