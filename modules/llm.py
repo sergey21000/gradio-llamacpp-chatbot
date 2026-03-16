e1307abe2e3d9bb56c4155d81dd5bf7daa0360dc
sergey21000 <67040429+sergey21000@users.noreply.github.com>
2026-03-16 15:07:04 +0300

    chore

--- modules/llm.py
+++ modules/llm.py
@@ -23,10 +23,10 @@ if openai_api_key or openai_base_url:
 else:
     priority_patterns = ['cuda'] if Config.LLAMACPP_PREFER_CUDA_BUILD else ['cpu']
     llama_server = LlamaSyncServer(
-        verbose=True,
-        release_manager=LlamaReleaseManager(
-            tag=Config.LLAMACPP_RELEASE_TAG,
-            priority_patterns=priority_patterns,
-        ),
+        # verbose=True,
+        # release_manager=LlamaReleaseManager(
+        #     tag=Config.LLAMACPP_RELEASE_TAG,
+        #     priority_patterns=priority_patterns,
+        # ),
     )
     llm_client = LlamaSyncClient(openai_base_url=llama_server.openai_base_url)
